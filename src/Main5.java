b80640b3292a3648efd2ae2220c9e7fc1eb9cf65
Bianca Milea <alumil2013@ieselcaminas.org>
2022-11-15 21:55:54 +0100

    Ioana Bianca Milea - Exercise 48

---
+++ src/Main5.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main5 {
+    public static void main(String[] args) {
+        List<Point> points = new ArrayList(List.of(
+                new Point(0, 0),
+                new Point(1, 1),
+                new Point(2, 2),
+                new Point(3, 3),
+                new Point(4, 4),
+                new Point(5, 5),
+                new Point(6, 6),
+                new Point(7, 7),
+                new Point(8, 8),
+                new Point(9, 9)
+        ));
+
+    }
+}
