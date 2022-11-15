baeb9fdd41ff8c6f4450043cb73f4e01c9ea03fc
Bianca Milea <alumil2013@ieselcaminas.org>
2022-11-15 15:02:17 +0100

    exercise 10-11

--- src/Point.java
+++ src/Point.java
@@ -22,4 +22,20 @@ public class Point {
     public String toString() {
         return "(" + x + ", " + y + ")";
     }
+
+    public Integer getX() {
+        return x;
+    }
+
+    public void setX(Integer x) {
+        this.x = x;
+    }
+
+    public Integer getY() {
+        return y;
+    }
+
+    public void setY(Integer y) {
+        this.y = y;
+    }
 }
