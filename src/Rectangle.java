20c7ddf498f858ae615d19701979783bc8f6c45f
Bianca Milea <alumil2013@ieselcaminas.org>
2022-11-15 21:31:45 +0100

    Ioana Bianca Milea - Exercise 31

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -2,4 +2,11 @@ public class Rectangle extends Point{
 
     private Integer width;
     private Integer height;
+
+    public Rectangle(){
+        super();
+        width = 0;
+        height = 0;
+    }
+
 }
