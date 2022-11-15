ff22f64b823682c84aebdb0bd626b55691c96a34
Bianca Milea <alumil2013@ieselcaminas.org>
2022-11-15 20:58:20 +0100

    Ioana Bianca Milea - Exercise 26

--- src/Polygon.java
+++ src/Polygon.java
@@ -1,7 +1,12 @@
+import java.util.HashSet;
 import java.util.Set;
 
 public class Polygon {
     Set<Point> points;
+
+    public Polygon (){
+        this.points = new HashSet<>();
+    }
 
     public Polygon(Set<Point> points) {
         this.points = points;
