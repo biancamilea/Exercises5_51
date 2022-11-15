737571f8488c8aaf4b3c2a2f7286039e44387a48
Bianca Milea <alumil2013@ieselcaminas.org>
2022-11-15 20:26:07 +0100

    Ioana Bianca Milea - Exercise 16

---
+++ src/Segment.java
@@ -0,0 +1,9 @@
+public class Segment {
+    Point startPoint;
+    Point endPoint;
+
+    public Segment(){
+        this.startPoint = new Point(0, 0);
+        this.endPoint = new Point(0, 0);
+    }
+}
