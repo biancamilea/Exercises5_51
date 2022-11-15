b77ed4ef02b07c969475a0223a340f7f74cecafe
Bianca Milea <alumil2013@ieselcaminas.org>
2022-11-15 21:40:30 +0100

    Ioana Bianca Milea - Exercise 36

--- src/Main4.java
+++ src/Main4.java
@@ -4,5 +4,11 @@ public class Main4 {
         System.out.println(rectangle.toString());
         rectangle.moveTo(4, 7);
         System.out.println(rectangle.toString());
+
+        Point point = new Point(1, 1);
+
+        Rectangle rectangle2 = new Rectangle(point, 8, 6);
+        System.out.println(rectangle2.toString());
+
     }
 }
