1166854db32bf07bd4410858b6db4d2e5a4dceae
Bianca Milea <alumil2013@ieselcaminas.org>
2022-11-15 21:34:15 +0100

    Ioana Bianca Milea - Exercise 32

---
+++ src/Main4.java
@@ -0,0 +1,8 @@
+public class Main4 {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle();
+        System.out.println(rectangle.toString());
+        rectangle.moveTo(4, 7);
+        System.out.println(rectangle.toString());
+    }
+}
