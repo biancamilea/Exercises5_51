989b08dce8e9260e377b5de1a8a8f126dd409dd1
Bianca Milea <alumil2013@ieselcaminas.org>
2022-11-15 14:54:27 +0100

    exercise 5

---
+++ src/Point.java
@@ -0,0 +1,15 @@
+public class Point {
+    Integer x;
+    Integer y;
+
+
+    public Point(){
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
